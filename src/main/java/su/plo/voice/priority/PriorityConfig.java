c6fa0ab95ffa31616c6cd1f42ba1b7960faf4153
Apehum <apehumchik@gmail.com>
2022-10-18 23:31:01 +0800

    feat: pv priority addon

---
+++ src/main/java/su/plo/voice/priority/PriorityConfig.java
@@ -0,0 +1,39 @@
+package su.plo.voice.priority;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import su.plo.config.Config;
+import su.plo.config.ConfigField;
+import su.plo.config.ConfigValidator;
+
+import java.util.function.Predicate;
+
+@Config
+@Data
+public final class PriorityConfig {
+
+    @ConfigField(path = "max_distance", comment = "Maximum priority distance")
+    @ConfigValidator(value = DistanceValidator.class, allowed = "1-1024")
+    private int maxDistance = 128;
+
+    @ConfigField(path = "default_distance", comment = "Default priority distance")
+    @ConfigValidator(value = DistanceValidator.class, allowed = "1-1024")
+    private int defaultDistance = 48;
+
+    @ConfigField(path = "activation_weight")
+    private int activationWeight = 10;
+
+    @ConfigField(path = "sourceline_weight")
+    private int sourceLineWeight = 10;
+
+    @NoArgsConstructor
+    public static class DistanceValidator implements Predicate<Object> {
+
+        @Override
+        public boolean test(Object o) {
+            if (!(o instanceof Long)) return false;
+            long port = (long) o;
+            return port > 0 && port <= 1024;
+        }
+    }
+}
