c6f4e9d398ceec163bcc3d92e16f77f527a985f5
Apehum <apehumchik@gmail.com>
2023-03-11 20:16:14 +0800

    feat: update API

--- src/main/java/su/plo/voice/priority/PriorityActivation.java
+++ src/main/java/su/plo/voice/priority/PriorityActivation.java
@@ -2,44 +2,43 @@ package su.plo.voice.priority;
 
 import com.google.common.collect.ImmutableList;
 import org.jetbrains.annotations.NotNull;
-import su.plo.lib.api.server.permission.PermissionDefault;
-import su.plo.voice.api.event.EventPriority;
-import su.plo.voice.api.event.EventSubscribe;
 import su.plo.voice.api.server.PlasmoVoiceServer;
-import su.plo.voice.api.server.audio.capture.BaseProximityServerActivation;
+import su.plo.voice.api.server.audio.capture.ProximityServerActivationHelper;
 import su.plo.voice.api.server.audio.capture.ServerActivation;
-import su.plo.voice.api.server.audio.source.ServerPlayerSource;
-import su.plo.voice.api.server.event.audio.source.PlayerSpeakEndEvent;
-import su.plo.voice.api.server.event.audio.source.PlayerSpeakEvent;
-import su.plo.voice.api.server.player.VoiceServerPlayer;
-import su.plo.voice.proto.packets.tcp.serverbound.PlayerAudioEndPacket;
-import su.plo.voice.proto.packets.udp.serverbound.PlayerAudioPacket;
+import su.plo.voice.api.server.audio.line.ServerSourceLine;
 
-public final class PriorityActivation extends BaseProximityServerActivation {
+public final class PriorityActivation  {
+
+    private final PlasmoVoiceServer voiceServer;
 
     private final PriorityAddon addon;
     private final PriorityConfig config;
 
-    private ServerActivation activation;
+    private ProximityServerActivationHelper proximityHelper;
 
     public PriorityActivation(@NotNull PlasmoVoiceServer voiceServer,
                               @NotNull PriorityAddon addon) {
-        super(voiceServer, "priority", PermissionDefault.OP);
-
+        this.voiceServer = voiceServer;
         this.addon = addon;
         this.config = addon.getConfig();
     }
 
     public void register() {
-        ServerActivation.Builder builder = getVoiceServer().getActivationManager().createBuilder(
+        if (proximityHelper != null) {
+            voiceServer.getEventBus().unregister(this, proximityHelper);
+            voiceServer.getActivationManager().unregister(proximityHelper.getActivation());
+            voiceServer.getSourceLineManager().unregister(proximityHelper.getSourceLine());
+        }
+
+        ServerActivation.Builder builder = voiceServer.getActivationManager().createBuilder(
                 addon,
-                getActivationName(),
+                "priority",
                 "pv.activation.priority",
                 "plasmovoice:textures/icons/microphone_priority.png",
                 "pv.activation.priority",
                 config.activationWeight()
         );
-        this.activation = builder
+        ServerActivation activation = builder
                 .setDistances(ImmutableList.of(-1, config.maxDistance()))
                 .setDefaultDistance(config.defaultDistance())
                 .setProximity(true)
@@ -47,48 +46,20 @@ public final class PriorityActivation extends BaseProximityServerActivation {
                 .setStereoSupported(config.activationStereoSupport())
                 .build();
 
-        getVoiceServer().getSourceLineManager().register(
+        ServerSourceLine sourceLine = voiceServer.getSourceLineManager().createBuilder(
                 addon,
-                getActivationName(),
+                "priority",
                 "pv.activation.priority",
                 "plasmovoice:textures/icons/speaker_priority.png",
                 config.sourceLineWeight()
+        ).build();
+
+        this.proximityHelper = new ProximityServerActivationHelper(
+                voiceServer,
+                activation,
+                sourceLine,
+                null
         );
-    }
-
-    @EventSubscribe(priority = EventPriority.HIGHEST)
-    public void onPlayerSpeak(@NotNull PlayerSpeakEvent event) {
-        if (activation == null) return;
-
-        VoiceServerPlayer player = (VoiceServerPlayer) event.getPlayer();
-        PlayerAudioPacket packet = event.getPacket();
-
-        if (!activation.checkPermissions(player) ||
-                packet.getDistance() > config.maxDistance() ||
-                packet.getDistance() <= 0
-        ) return;
-
-        ServerPlayerSource source = getPlayerSource(player, packet.getActivationId(), packet.isStereo());
-        if (source == null) return;
-
-        sendAudioPacket(player, source, packet, packet.getDistance());
-    }
-
-    @EventSubscribe(priority = EventPriority.HIGHEST)
-    public void onPlayerSpeakEnd(@NotNull PlayerSpeakEndEvent event) {
-        if (activation == null) return;
-
-        VoiceServerPlayer player = (VoiceServerPlayer) event.getPlayer();
-        PlayerAudioEndPacket packet = event.getPacket();
-
-        if (!activation.checkPermissions(player) ||
-                packet.getDistance() > config.maxDistance() ||
-                packet.getDistance() <= 0
-        ) return;
-
-        ServerPlayerSource source = getPlayerSource(player, packet.getActivationId(), null);
-        if (source == null) return;
-
-        sendAudioEndPacket(source, packet, packet.getDistance());
+        voiceServer.getEventBus().register(addon, proximityHelper);;
     }
 }
