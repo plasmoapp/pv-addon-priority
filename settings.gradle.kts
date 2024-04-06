d4f2a490e0c107940a9614fe0cc273706cc095f2
Apehum <apehumchik@gmail.com>
2024-04-06 16:37:42 +0800

    build: add plasmo releases repo to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,8 +4,8 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven("https://repo.plasmoverse.com/snapshots")
+        maven("https://repo.plasmoverse.com/releases")
         maven("https://jitpack.io/")
-        maven("https://maven.minecraftforge.net")
     }
 }
 
